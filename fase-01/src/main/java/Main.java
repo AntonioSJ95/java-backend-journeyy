3cb598d68b515fb173a26e28b6ac437c0ded2891
Antonio <antonioseinjagou95@gmail.com>
2026-03-09 19:32:44 -0600

    Fase-01: Hello World Inicial - sesion 0 completada

---
+++ fase-01/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java Backend Journey - Iniciando");
+        System.out.println("Desarrollador: Antonio");
+        System.out.println("Fase: 01 - Java desde cero");
+    }
+}
