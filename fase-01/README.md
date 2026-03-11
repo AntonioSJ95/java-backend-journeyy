dcdc0da08ab70deadedbae24f2882337c5fa6134
Antonio <antonioseinjagou95@gmail.com>
2026-03-11 12:49:41 -0600

    fase-01: ejercicio 2 - calculadora de propina completado

---
+++ fase-01/README.md
@@ -0,0 +1,15 @@
+# Fase 01 — Java desde Cero
+
+## ¿Qué estoy aprendiendo aquí?
+- Sintaxis y tipos de datos
+- POO (Programación Orientada a Objetos)
+- Colecciones y Java moderno
+- Testing con JUnit 5
+
+## Ejercicios
+- [ ] Ejercicio 1.1 — Calculadora de IMC
+- [ ] Ejercicio 1.2 — Sistema de Inventario
+- [ ] Ejercicio 1.3 — Gestor de Tareas CLI
+
+## Estado
+🟡 En progreso
--- fase-01/src/main/java/Main.java
+++ fase-01/src/main/java/Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java Backend Journey - Iniciando");
-        System.out.println("Desarrollador: Antonio");
-        System.out.println("Fase: 01 - Java desde cero");
+       propina();
+
+    }
+
+    public static void calentamiento(){
+        String name = "Antonio";
+        int cuenta = 12345;
+        double saldo = 1250.50;
+        boolean activa = true;
+
+        String formattedString = String.format("Nombre: %s | Cuenta: %d | Saldo: $%.2f | Activa: %b" , name,cuenta,saldo,activa);
+        System.out.println(formattedString);
+    }
+
+    public static void propina(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce la cuenta a pagar");
+        int cuenta = sc.nextInt();
+        System.out.println("Introduce la propia a agregar a la cuenta: 10, 15 o 20");
+        int propina =  sc.nextInt();
+        double totalPropina = (cuenta * (propina / 100.0));
+        System.out.println(String.format("La propina es: $%.2f", totalPropina));
+        System.out.println(String.format("Total a pagar: $%.2f", cuenta + totalPropina));
     }
 }
